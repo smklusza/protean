93ad0f6e84972c0dfd51a0a960d5c30f62f48f6a
Stephen Klusza <smklusza@gmail.com>
2025-12-12 19:05:54 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,24 +10,28 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y emacs
+dnf5 -y install emacs
 dnf5 -y install swaylock
 dnf5 -y install mako
 dnf5 -y install mousepad
 dnf5 -y install grim
 dnf5 -y install slurp
 dnf5 -y install fuzzel
+dnf5 -y install thunar
+dnf5 -y install thunar-volman
+dnf5 -y remove konsole
+dnf5 -y remove dolphin
 
 # Grab scroll and sway:
-dnf5 -y copr enable mecattaf/duoRPM
-dnf5 -y install scroll
-dnf5 -y copr disable mecattaf/duoRPM
+# dnf5 -y copr enable mecattaf/duoRPM
+# dnf5 -y install scroll
+# dnf5 -y copr disable mecattaf/duoRPM
 
 # Grab MangoWC
-dnf5 -y copr enable dennemann/MangoWC
-dnf5 -y install mangowc
-dnf5 -y install scenefx
-dnf5 -y copr disable dennemann/MangoWC
+# dnf5 -y copr enable dennemann/MangoWC
+# dnf5 -y install mangowc
+# dnf5 -y install scenefx
+# dnf5 -y copr disable dennemann/MangoWC
 
 # Grab Quickshell
 dnf5 -y copr enable mecattaf/packages
